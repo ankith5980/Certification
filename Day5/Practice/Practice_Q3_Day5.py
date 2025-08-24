b375f6322897e564667da8469f46b21810abc9df
ankith5980 <ankithpratheesh147@gmail.com>
2025-08-24 18:06:12 +0530

    Practice Q3 solved

---
+++ Day5/Practice/Practice_Q3_Day5.py
@@ -0,0 +1,24 @@
+# import numpy as np
+
+# # Coefficient matrix
+# A = np.array([[2, 3],
+#               [5, 4]])
+
+# # Constants
+# b = np.array([8, 14])
+
+# # Ax = b
+# solution = np.linalg.solve(A, b)
+
+# x, y = solution
+# print(f"x = {x}, y = {y}")
+
+# print("\n")
+# print("========================")
+# print("\n")
+
+# import numpy as np
+# A = np.array([[4, -2],
+#               [1,  1]])
+# w, V = np.linalg.eig(A)
+# w, V  # w ≈ [3., 2.], columns of V are eigenvectors
