e87831d42d5506a504c8734cb89fd95094694538
ankith5980 <ankithpratheesh147@gmail.com>
2025-08-27 16:45:35 +0530

    Bar Graph using sea born

---
+++ Day6/seaborn_error_bar.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+data = pd.DataFrame({
+    "category" : ['A', 'B', 'C', 'D'],
+    "value" : [4, 7, 2, 9]
+})
+sns.barplot(x="category", y="value", data=data)
+plt.title("Normal Bar Chart (SeaBorn)")
+plt.show()
