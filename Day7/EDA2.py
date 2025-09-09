0f73c3b8892bc42fcfd7c918b72ef3cf1003660e
ankith5980 <ankithpratheesh147@gmail.com>
2025-09-09 20:56:40 +0530

    Minor changes

--- Day7/EDA2.py
+++ Day7/EDA2.py
@@ -5,5 +5,6 @@ data = pd.read_csv("Day7/MISSING_DATASET_HANDLING.csv", encoding = "latin1")
 print(data.isnull().sum())
 
 imputer = KNNImputer(n_neighbors=5)
-data_imputed = pd.DataFrame(imputer.fit_transform(data.select_dtypes(include=['float64', 'int64'])), columns=data.select_dtypes(include=['float64', 'int64']).columns)
+data_imputed = pd.DataFrame(imputer.fit_transform(data.select_dtypes(include=['float64', 'int64'])), 
+                            columns=data.select_dtypes(include=['float64', 'int64']).columns)
 print(data_imputed.isnull().sum())
